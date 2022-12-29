c44456cccb4a1848dd81842192645bee8f117917
ramon <ramon.piologo@santodigital.com.br>
2022-12-29 11:32:05 -0300

    exercicio 22

--- funcoes/exercicio_22.py
+++ funcoes/exercicio_22.py
@@ -3,3 +3,21 @@
 # Considere um intervalo de 1 a 20.
 
 [(0, 1), (1, 4), (2, 9), (3, 16), (4, 25), (5, 36), (6, 49), (7, 64), (8, 81), (9, 100), (10, 121), (11, 144), (12, 169), (13, 196), (14, 225), (15, 256), (16, 289), (17, 324), (18, 361), (19, 400)]
+
+
+
+
+
+
+lista = []
+lista2 = []
+listafinal=[]
+for i in range(1,21):
+    lista.append(i)
+    lista2.append(i*i)
+    listafinal = zip(lista,lista2)
+
+print(list(listafinal))
+
+
+print(list(zip(range(20),[x**2 for x in range(1,21)])))
