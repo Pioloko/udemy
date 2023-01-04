68127489a874af80b0bc380f4f0d29c83898c38b
ramon <ramon.piologo@santodigital.com.br>
2023-01-04 10:54:52 -0300

    asa

--- projetos/Projeto Acerte o Número.py
+++ projetos/Projeto Acerte o Número.py
@@ -4,3 +4,30 @@
 # Se o usuário acertar o número aleatório mostre uma mensagem personalizada na tela, a cada laço de repetição.
 # E caso o usuário não acerte, mostre na tela se o número que ele digitou foi maior ou menor que o número aleatório.
 # Ao final mostre a quantidade de acertos.
+
+from random import randint
+acertos=0
+ 
+for num in range(1,16):
+    print(f'Jogada {num}')
+    numero=randint(1,50)
+    escolha_usuario=int(input('Digite um número entre 1-50: '))
+    
+    if escolha_usuario in range(1,51):
+        if escolha_usuario>numero:
+            print('Você Digitou um número maior que o aleatório')
+            print(f'Número Aleatório: {numero}')
+            
+        elif escolha_usuario<numero:
+            print('Você digitou um número menor que o aleatório')
+            print(f'Número Aleatório: {numero}')
+            
+        else:
+            print('Você acertou!')
+            acertos+=1
+    
+    else:
+        print('Fim.')
+        break
+        
+print(f'Número de acertos: {acertos}')
